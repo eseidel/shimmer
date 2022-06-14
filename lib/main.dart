6b1f43982ecd001fa585b8b73d37ce61c8dd1f10
Eric Seidel <github@eseidel.com>
2022-06-13 22:33:22 -0400

    Draws obstacle images correcxtly
    Now just needs to respect obstacles!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:shimmer/geometry.dart';
 import 'package:image/image.dart' as img;
+import 'dart:ui' as ui;
 
 import 'grid.dart';
 import 'package:flame/components.dart';
@@ -121,26 +122,20 @@ class TapIndicator extends PositionComponent {
 class ShimmerGame extends FlameGame with TapDetector {
   late Player player;
 
-  Future<Grid<bool>> obstacleGridFromImage(img.Image image) async {
-    return Future.value(
-        Grid.filled(ISize(image.height, image.height), (GridPosition position) {
-      var color = image.getPixel(position.x, position.y);
-      // Treat darker pixels as obstacles, lighter as empty.
-      return img.getLuminance(color) < 128;
-    }));
-  }
-
   @override
   Future<void> onLoad() async {
+    debugMode = true;
     var data = await rootBundle.load('assets/images/checker-4x4.png');
     final buffer = data.buffer;
     var ints = buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
     var mapImage = img.decodeImage(ints)!;
-    final obstacleGrid = await obstacleGridFromImage(mapImage);
-    var map = NewMap(obstacleGrid);
+    var obstacleGrid = NewMap.obstacleGridFromImage(mapImage);
+    var obstacleImage = await ImageExtension.fromPixels(
+        mapImage.getBytes(), mapImage.width, mapImage.height);
+    var map = NewMap(obstacleGrid, obstacleImage);
     add(map);
     add(player = Player(size: 50, position: size / 2));
-    camera.followComponent(player, worldBounds: map.bounds);
+    camera.followComponent(player);
   }
 
   @override
@@ -154,40 +149,41 @@ class ShimmerGame extends FlameGame with TapDetector {
 class NewMap extends Component {
   static const double multiplier = 100;
   Grid<bool> obstacleGrid;
-  Rect bounds;
+  late Rect bounds;
+  ui.Image obstacleImage;
 
-  static final Paint _paintObstacle = Paint()
-    ..color = Colors.orange
-    ..style = PaintingStyle.fill;
+  static Grid<bool> obstacleGridFromImage(img.Image image) {
+    return Grid.filled(ISize(image.height, image.height),
+        (GridPosition position) {
+      var color = image.getPixel(position.x, position.y);
+      // Treat darker pixels as obstacles, lighter as empty.
+      return img.getLuminance(color) < 128;
+    });
+  }
 
   static final Paint _paintBorder = Paint()
     ..color = Colors.white12
     ..strokeWidth = 10
     ..style = PaintingStyle.stroke;
   static final Paint _paintBg = Paint()..color = const Color(0xFF333333);
+  static final Paint _paintImage = Paint()
+    ..color = const Color.fromRGBO(0, 0, 0, 0.5);
 
-  late final List<Rect> _rectPool;
-
-  NewMap(this.obstacleGrid)
-      : bounds = Rect.fromLTWH(0, 0, (obstacleGrid.width + 1) * multiplier,
-            (obstacleGrid.height + 1) * multiplier),
-        super(priority: 0) {
-    _rectPool = [];
-    for (var position in obstacleGrid.allPositions) {
-      if (obstacleGrid.get(position) == true) {
-        _rectPool.add(Rect.fromLTWH(multiplier * position.x,
-            multiplier * position.y, multiplier, multiplier));
-      }
-    }
+  NewMap(this.obstacleGrid, this.obstacleImage) : super(priority: 0) {
+    bounds = Rect.fromLTWH(0, 0, (obstacleGrid.width) * multiplier,
+        (obstacleGrid.height) * multiplier);
   }
 
   @override
   void render(Canvas canvas) {
     canvas.drawRect(bounds, _paintBg);
     canvas.drawRect(bounds, _paintBorder);
-    for (var rect in _rectPool) {
-      canvas.drawRect(rect, _paintObstacle);
-    }
+    canvas.drawImageRect(
+        obstacleImage,
+        Rect.fromLTWH(0, 0, obstacleImage.width.toDouble(),
+            obstacleImage.height.toDouble()),
+        bounds,
+        _paintImage);
   }
 }
 
